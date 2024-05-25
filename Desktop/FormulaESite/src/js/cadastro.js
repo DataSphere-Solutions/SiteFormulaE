c055fafb7debe42724687f04aa1015507a43a96c
artur tenca <arturtenca1@gmail.com>
2024-05-25 00:20:06 -0300

    mudando css sa pagina de cadastro

--- Desktop/FormulaESite/src/js/cadastro.js
+++ Desktop/FormulaESite/src/js/cadastro.js
@@ -4,20 +4,8 @@ function validar() {
     
     if (user == 'user' && senha == 1234) {
         window.alert('Usuário validado!')
-        window.location.href = "./quiz.html";
+        window.location.href = "./index.html";
     } else if (user != 'user' || senha != 1234) {
         window.alert('Usuário ou senha inválidos! Preencha os dados e tente novamente.')
-    }
-}
-
-function formulario() {
-    let nome = document.querySelector('#nomehtml').value;
-    let telefone = document.querySelector('#telhtml').value;
-    let email = document.querySelector('#emailhtml').value;
-
-    if (nome == '' || telefone == '' || email == '') {
-        window.alert('Erro! Algum campo não foi preenchido. Verifique os dados e tente novamente.')
-    } else {
-        window.alert('Dados enviados com sucesso!')
     }
 }

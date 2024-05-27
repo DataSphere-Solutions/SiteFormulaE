f7555360d7a21ab1d79140ac6037231241520fe6
artur tenca <arturtenca1@gmail.com>
2024-05-26 23:29:37 -0300

    adicionando section oculta na pagina de corridas

---
+++ Desktop/FormulaESite/src/js/corridas.js
@@ -0,0 +1,44 @@
+document.getElementById('btn1').addEventListener('click', function() {
+    var section = document.getElementById('sec1');
+    if (section.classList.contains('hidden')) {
+        section.classList.remove('hidden');
+    } else {
+        section.classList.add('hidden');
+    }
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+    var section = document.getElementById('sec2');
+    if (section.classList.contains('hidden')) {
+        section.classList.remove('hidden');
+    } else {
+        section.classList.add('hidden');
+    }
+});
+
+document.getElementById('btn3').addEventListener('click', function() {
+    var section = document.getElementById('sec3');
+    if (section.classList.contains('hidden')) {
+        section.classList.remove('hidden');
+    } else {
+        section.classList.add('hidden');
+    }
+});
+
+document.getElementById('btn4').addEventListener('click', function() {
+    var section = document.getElementById('sec4');
+    if (section.classList.contains('hidden')) {
+        section.classList.remove('hidden');
+    } else {
+        section.classList.add('hidden');
+    }
+});
+
+document.getElementById('btn5').addEventListener('click', function() {
+    var section = document.getElementById('sec5');
+    if (section.classList.contains('hidden')) {
+        section.classList.remove('hidden');
+    } else {
+        section.classList.add('hidden');
+    }
+});
